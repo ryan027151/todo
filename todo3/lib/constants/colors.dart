eea8b466c425bd2ff8204f2cf92cda1ef1f18d5d
ryan027151 <ryan027151@gmail.com>
2023-07-31 12:56:51 -0400

    basic

---
+++ todo3/lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color charcoal = Color(0xFF6f6f6d) ;
+
+const Color duestyBlue = Color(0xFFb9bfc4); 
+
+const Color cashmere = Color(0xFFd1c8c7); 
+
+const Color denim = Color(0xFF7b8698);
---
+++ todo3/lib/screens/home.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:todo3/constants/colors.dart';
+
+
+class Home extends StatelessWidget{
+  const Home({Key? key}) : super(key:key);
+
+  @ override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: charcoal,
+      appBar:_buildAppBar(),//AppBar
+      body:Container(
+        child:Column(
+          children:[
+            Expanded(
+            child:ListView(
+              children: [
+                Container(
+                  margin:EdgeInsets.only(
+                    top:50,
+                    bottom:20,
+                    ), //EdgeInsets.only
+                  child:Text(
+                    "To do",
+                     style:TextStyle(
+                      fontSize:30,
+                      fontWeight: FontWeight.w500,
+                    ), //Textstyle
+                  ), //Text
+                ), //Container
+              ],
+            ) //Listview
+            ) //Expanded
+          ],
+        ),//Column
+      ), //Container
+    );//Scaffold
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor:charcoal,
+      elevation:0,
+      title:Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+        const Icon(
+          Icons.menu,
+          color:cashmere,
+          size:30,
+        ),//Icon
+        Container(
+          height:40,
+          width:40,
+          child:ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child:Image.asset('assets/images/avatar.jpeg'),
+          ),//ClipRRect
+        ),//Container
+      ]),//Row
+    );
+  }
+}
