515186a98ef581b6cdc5b14fb8b55f0e1b30f2b9
ryan027151 <ryan027151@gmail.com>
2023-08-01 12:54:44 -0400

    update

--- todo3/lib/model/todo.dart
+++ todo3/lib/model/todo.dart
@@ -9,11 +9,23 @@ class ToDo {
     this.isDone = false,
   });
 
-  static Set<ToDo> todoList() {
-    return {
-      ToDo(id:'01',todoText:'Type your todo'),
-      ToDo(id:'02',todoText:'Type your todo'),
-      ToDo(id:'03',todoText:'Type your todo')
-    };
+  ToDo copyWith({
+    String? id,
+    String? todoText,
+    bool? isDone,
+  }) {
+    return ToDo(
+      id: id ?? this.id,
+      todoText: todoText ?? this.todoText,
+      isDone: isDone ?? this.isDone,
+    );
+  }
+
+  static List<ToDo> todoList() {
+    return [
+      ToDo(id: '01', todoText: 'Type your todo'),
+      ToDo(id: '02', todoText: 'Type your todo'),
+      ToDo(id: '03', todoText: 'Type your todo'),
+    ];
   }
 }
--- todo3/lib/widgets/todo_item.dart
+++ todo3/lib/widgets/todo_item.dart
@@ -2,37 +2,88 @@ import 'package:flutter/material.dart';
 import 'package:todo3/constants/colors.dart';
 import '../model/todo.dart';
 
-class ToDoItem extends StatelessWidget {
+class ToDoItem extends StatefulWidget {
   final ToDo todo;
-  final onToDoChanged;
-
+  final  onToDoChanged;
   const ToDoItem({Key? key, required this.todo, required this.onToDoChanged})
       : super(key: key);
 
   @override
+  _ToDoItemState createState() => _ToDoItemState();
+}
+
+class _ToDoItemState extends State<ToDoItem> {
+  final TextEditingController _textEditingController = TextEditingController();
+  bool _isEditing = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController.text = widget.todo.todoText!;
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      child: ListTile(
-        onTap: () {
-          onToDoChanged(todo);
-        },
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ), //RoundedRectangleBorder
-        tileColor: charcoal,
-        leading: Icon(
-          todo.isDone ? Icons.check_box :Icons.check_box_outline_blank,
-          color: denim,
-        ),
-        title: Text(
-          todo.todoText!,
-          style:TextStyle(
-            fontSize: 16,
-            color: Color.fromRGBO(149, 149, 148, 1),
-            decoration: todo.isDone? TextDecoration.lineThrough:null,
-          ), //Textstyle
-        ), //Text
-      ), //Listile
-    ); //Container
+      child: Row(
+        children: [
+          Checkbox(
+            value: widget.todo.isDone,
+            onChanged: (bool? newValue) {
+              widget.onToDoChanged(widget.todo.copyWith(isDone: newValue));
+            },
+            activeColor: denim,
+          ),
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  _isEditing = true;
+                });
+              },
+              child: _isEditing
+                  ? TextField(
+                      controller: _textEditingController,
+                      onSubmitted: (value) {
+                        setState(() {
+                          _isEditing = false;
+                          widget.onToDoChanged(
+                            widget.todo.copyWith(todoText: value),
+                          );
+                        });
+                      },
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: const Color.fromRGBO(149, 149, 148, 1),
+                        decoration:
+                            widget.todo.isDone ? TextDecoration.lineThrough : null,
+                      ),
+                    )
+                  : ListTile(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      tileColor: charcoal,
+                      title: Text(
+                        widget.todo.todoText!,
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: const Color.fromRGBO(149, 149, 148, 1),
+                          decoration:
+                              widget.todo.isDone ? TextDecoration.lineThrough : null,
+                        ),
+                      ),
+                    ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
